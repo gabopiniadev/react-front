2b747e1a625a3542f98f641edbfd108d023bc8b2
Gabriel Alejandro Piña <89942339+gabopiniadev@users.noreply.github.com>
2022-10-12 09:50:18 -0400

    Update content.jsx

--- src/components/pages/Contein/content.jsx
+++ src/components/pages/Contein/content.jsx
@@ -1,29 +1,44 @@
 import Table from 'react-bootstrap/Table';
+import React, {useEffect, useState} from 'react';
 
 function Listoperator() {
+  const url = 'http://localhost:8080/v1/lista-proveedores/1/17/'
+  const [proveedores, setProveedores] = useState()
+  const fetchApi = async () => {
+    const response = await fetch(url)
+    const responseJSON = await response.json()
+    setProveedores(responseJSON)
+  }
+  useEffect(() => {
+    fetchApi()
+  }, [])
     return (
         <Table striped bordered hover>
           <thead>
             <tr>
               <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
+              <th>Nombre</th>
+              <th>Rif</th>
+              <th>Direccion</th>
+              <th>Razon Social</th>
               <th>Acciones</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+          {!proveedores ? 'Cargando...' : 
+         proveedores.map( ( proveedor, index) => {
+            return <tr>
+              <td>#</td>
+              <td>{proveedor.nombre}</td>
+              <td>{proveedor.rif}</td>
+              <td>{proveedor.direccion}</td>
+              <td>{proveedor.razonSocial}</td>
               <td></td>
             </tr>
+            })}
           </tbody>
         </Table>
       );
     }
 
 export default Listoperator;
-
